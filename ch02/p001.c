f057188e858a11ca1285795fcf90384f6c3e210f
Gresocs <43661383+Gresocs@users.noreply.github.com>
2019-11-22 14:55:04 +0800

    Update p001.c

--- ch02/p001.c
+++ ch02/p001.c
@@ -1,1 +1,13 @@
+/* practice 001 */
 
+#include <stdio.h>
+
+int main(void)
+{
+  printf("Gustav Mahler\n");
+  printf("Gustav\nMahler\n");
+  printf("Gustav");
+  printf("Mahler\n");
+  
+  return 0;
+}
