d02fdc5f0717e55320ad5f195a96e4e04b5173d0
Gresocs <43661383+Gresocs@users.noreply.github.com>
2019-12-04 14:43:32 +0800

    Update p006.c

--- ch02/p006.c
+++ ch02/p006.c
@@ -1,1 +1,11 @@
+/* practice 006 */
 
+#include <stdio.h>
+
+int main(void)
+{
+  int toes = 10;
+  printf(" toes is %d.\n twice of toes is %d.\n square of toes is %d.\n", toes, 2*toes, toes*toes);
+  
+  return 0;
+}
