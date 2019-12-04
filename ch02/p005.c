cb95e9d43bcc64818c72fcd46c8c9803400352b4
Gresocs <43661383+Gresocs@users.noreply.github.com>
2019-12-04 14:35:33 +0800

    Update p005.c

--- ch02/p005.c
+++ ch02/p005.c
@@ -1,1 +1,26 @@
+/* practice 005 */
 
+#include <stdio.h>
+
+void br(void);
+void ic(void);
+
+int main(void)
+{
+  printf("Brazil, Russia, ");
+  ic();
+  printf("India, China,\n");
+  br();
+  
+  return 0;
+}
+
+void br(void)
+{
+  printf("Brazil, Russia\n");
+}
+
+void ic(void)
+{
+  printf("India, China\n");
+}
